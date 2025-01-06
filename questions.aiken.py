5dd7a4e4b03c809a5ecf53cafa83258c70b7cbab
jeevan-cpu <anumandlajeevansai7@gmail.com>
2025-01-06 21:40:24 +0530

    Add files via upload

---
+++ questions.aiken.py
@@ -0,0 +1,20 @@
+What is the capital of Germany?
+A) Paris
+B) Berlin
+C) Madrid
+D) Rome
+ANSWER: B
+
+What is 2 + 2?
+A) 3
+B) 4
+C) 5
+D) 6
+ANSWER: B
+
+Which colors are in the rainbow?
+A) Red
+B) Blue
+C) Green
+D) Black
+ANSWER: A,B,C
